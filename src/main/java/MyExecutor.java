62fced3e1d867a1c9aeca9120abf06d79243ea49
fandanping <18602474229@163.com>
2018-08-20 11:20:58 +0800

    Initial commit

---
+++ src/main/java/MyExecutor.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Executors.newFixedThreadPool()
+ * newFixedThreadPool 创建一个定长线程池，可控制线程最大并发数，超出的线程会在队列中等待。
+ */
+public class MyExecutor  extends Thread{
+     private int index;
+     public MyExecutor(int i){
+         this.index=i;
+     }
+     public void run(){
+         try {
+             System.out.println("["+this.index+"] start..." );
+             Thread.sleep((int)(Math.random()*10000));
+             System.out.println("["+this.index+"] end..." );
+         } catch (InterruptedException e) {
+             e.printStackTrace();
+         }
+     }
+     public static void main(String[] args){
+         ExecutorService service= Executors.newFixedThreadPool(4);
+         for(int i=0;i<10;i++){
+             service.execute(new MyExecutor(i));
+         }
+         System.out.println("submit finish");
+         service.shutdown();
+     }
+
+}
---
+++ src/main/java/TestCountDownLatch.java
@@ -0,0 +1,35 @@
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TestCountDownLatch {
+    public static void main(String[] args) throws InterruptedException {
+        final CountDownLatch begin=new CountDownLatch(1);
+        final CountDownLatch end=new CountDownLatch(10);
+
+        final ExecutorService exec= Executors.newFixedThreadPool(10);
+        for(int index=0;index<10;index++){
+            final int NO=index +1;
+            Runnable run=new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        begin.await();
+                        Thread.sleep((long)(Math.random()*10000));
+                        System.out.println("NO."+NO+"arrived");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }finally{
+                        end.countDown();
+                    }
+                }
+            };
+            exec.submit(run);
+        }
+        System.out.println("Game start");
+        begin.countDown();
+        end.await();
+        System.out.println("Game over");
+        exec.shutdown();
+    }
+}
